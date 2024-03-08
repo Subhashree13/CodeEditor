45f2ce342f75733ca74f65331a694428e029b1d5
Subhashree13 <shreesubha.p@gmail.com>
2024-03-08 16:34:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = ()=>{
+    return (<>Hello World!</>)
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App/>
+);
